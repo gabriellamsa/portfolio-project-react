7cfde39b42dc13df19b59d48e0a37964c3180a4c
Gabriella Andrade <gabii.marcelly@gmail.com>
2024-07-05 14:47:27 +0100

    Logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import BucketListLogo from '../img/bucket-list-logo.jpeg';
 import UserLoginForm from './UserLoginForm';
 
 const Header = () => {
   return (
     <Navbar light style={{ backgroundColor: 'white' }}> 
       <div className="container">
-        <NavbarBrand href="/">Bucket List - Project!</NavbarBrand>
+        <NavbarBrand href="/">
+          <img 
+            src={BucketListLogo}
+            alt='Bucket List Logo'
+            style={{ width: '50px', marginRight: '10px' }} />
+        </NavbarBrand>
         <UserLoginForm />
       </div>
     </Navbar>
